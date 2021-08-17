b8e7248ecad04367a8448c8f8bf24a7753d92fd8
people92 <eminjae8659@gmail.com>
2021-08-17 16:34:39 +0900

    json 배열인지 String인지 체크 유틸 추가

--- src/main/java/com/people/utils/JsonUtils.java
+++ src/main/java/com/people/utils/JsonUtils.java
@@ -1,6 +1,9 @@
 package com.people.utils;
 
 import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+import com.google.gson.JsonPrimitive;
 
 import java.lang.reflect.Type;
 
@@ -20,4 +23,13 @@ public class JsonUtils {
         Gson gson = new Gson();
         return gson.fromJson(json, clazz);
     }
+    public static boolean isJsonArray(String json) {
+        Gson gson = new Gson();
+        JsonElement jsonElement = gson.fromJson(json, JsonElement.class);
+        if(jsonElement.isJsonArray()) {
+            return true;
+        }
+        return false;
+    }
+
 }
